8cb4aa02f2cc2b38ecaafbc9597884e6ca5e8f4e
Allan Lancioni (WSL) <aclancioni@gmail.com>
2025-01-04 15:22:48 -0300

    feat: adding Lambda, API and Data stacks

--- src/infra/stacks/DataStack.ts
+++ src/infra/stacks/DataStack.ts
@@ -1,13 +1,22 @@
-import { Stack, StackProps } from "aws-cdk-lib"
+import { Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
-
+import { getSuffixFromStack } from "../utils/NamingUtils";
 
 export class DataStack extends Stack {
+  public readonly spacesTable: ITable;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+    const suffix = getSuffixFromStack(this);
 
+    this.spacesTable = new Table(this, "SpacesTable", {
+      tableName: `SpaceTable-${suffix}`,
+      partitionKey: {
+        name: "Id",
+        type: AttributeType.STRING,
+      },
+    });
+  }
 }
