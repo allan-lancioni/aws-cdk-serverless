d64353608522c2cfa0f33736cc76b2aed399bc56
Allan Lancioni (WSL) <aclancioni@gmail.com>
2025-01-04 17:00:43 -0300

    feat: adding debugger and communicating with AWS S3

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,13 +1,11 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 interface LambdaStackProps extends StackProps {
   spacesTable: ITable;
@@ -19,14 +17,23 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "HelloLambda", {
+    const helloLambda = new NodejsFunction(this, "HelloLambda", {
       runtime: Runtime.NODEJS_22_X,
-      handler: "hello.main",
-      code: Code.fromAsset(join(__dirname, "../../services")),
+      handler: "handler",
+      entry: join(__dirname, "../../services/hello.ts"),
       environment: {
         TABLE_NAME: props.spacesTable.tableName,
       },
     });
+
+    helloLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "s3:ListAllMyBuckets",
+        "s3:ListBucket",
+      ],
+      resources: ["*"],
+    }));
 
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
